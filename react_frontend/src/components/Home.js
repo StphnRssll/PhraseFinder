c39aa8fedb0f30cfc15df85b350c0ea6aee06838
StphnRssll <steve@Swift-SDR.localdomain>
2019-08-16 13:41:44 -0400

    Updated home page to have more example use cases

--- react_frontend/src/components/Home.js
+++ react_frontend/src/components/Home.js
@@ -7,10 +7,13 @@ function Home(props) {
             Search YouTube videos while searching captions within them
         </p>
         <p>
-            For example, <a href="/results?capQuery=trump&vidQuery=congressional%20hearing">click here to search for everytime "Trump" is said during Robert Muller's congressional hearing</a>
+            For example, <a href="/results?capQuery=tesla&vidQuery=electricity%20documentary">click here to search for everytime "<b>Tesla</b>" is mentioned during a documentary on the <b>history of electricity</b></a>
         </p>
         <p>
-            Or <a href="/results?capQuery=flavor&vidQuery=Guy%20Fieri">click here to search for everytime Guy Fieri says "flavor"</a>
+            Or <a href="/results?capQuery=flavor&vidQuery=Guy%20Fieri">click here to search for everytime <b>Guy Fieri</b> says "<b>flavor</b>"</a>
+        </p>
+        <p>
+            Or <a href="/results?capQuery=malaria&vidQuery=bill%20gates%20philanthropy">click here to search for the word "<b>malaria</b>" across videos about <b>Bill Gates' Philanthropy</b> </a>
         </p>
     </div>
   )
