ecc8b9d41b32b53d978fb3e952e3e341825732f6
StphnRssll <44075075+StphnRssll@users.noreply.github.com>
2020-09-20 16:46:16 -0400

    Update Home.js

--- react_frontend/src/components/Home.js
+++ react_frontend/src/components/Home.js
@@ -12,7 +12,7 @@ function Home(props) {
             For example, <a href="/results?capQuery=malaria&vidQuery=bill%20gates%20philanthropy">click here to search for the word "<b>malaria</b>" across videos about <b>Bill Gates' Philanthropy</b> </a>
         </p>
         <p>
-            Or <a href="http://e7cce6c0.ngrok.io/results?capQuery=batter&vidQuery=tesla%20shareholder%20meeting">click here to search for each time <b>batteries</b> are discussed in <b>tesla shareholder meetings</b></a>
+            Or <a href="/results?capQuery=batter&vidQuery=tesla%20shareholder%20meeting">click here to search for each time <b>batteries</b> are discussed in <b>tesla shareholder meetings</b></a>
         </p>
         <p>
             Or <a href="/results?capQuery=flavor&vidQuery=Guy%20Fieri">click here to search for everytime <b>Guy Fieri</b> says "<b>flavor</b>"</a>
