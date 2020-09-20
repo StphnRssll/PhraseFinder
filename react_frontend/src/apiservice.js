bc8750ef4cef6af4a74972acbd0de3d0886e2da3
StphnRssll <44075075+StphnRssll@users.noreply.github.com>
2020-09-20 17:03:13 -0400

    Update apiservice.js

--- react_frontend/src/apiservice.js
+++ react_frontend/src/apiservice.js
@@ -4,6 +4,6 @@ const API_URL = "https://stphnrssll-phrasefinder.zeet.app";
 //const config = {};
 
 function getVideoSearch(vid_query,caption_query){
-  return axios.get(`${API_URL}/results?vid_query=${vid_query}&caption_query=${caption_query}`);
+  return axios.get(`${API_URL}/newResults?vid_query=${vid_query}&caption_query=${caption_query}`);
 }
 export { getVideoSearch };
